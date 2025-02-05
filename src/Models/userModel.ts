3b634f57e8dd0eb44960ae7e312be464fffb11dd
Manikant Kumar <123896779+Manikantkr-1004@users.noreply.github.com>
2025-02-05 16:00:43 +0530

    Delete src directory

--- src/Models/userModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchama = new mongoose.Schema({
-    name: {type: String, required: true, trim: true},
-    email: {type: String, required: true, trim: true, unique: true},
-    password: {type: String, required: true, trim: true}
-}, {timestamps: true, versionKey: false});
-
-export const userModel = mongoose.model("user", userSchama);
--- src/Routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { UserAuth, UserLogin, UserLogout, UserRegister } from "../Controller/userController";
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.route("/register").post(UserRegister);
-userRouter.route("/login").post(UserLogin);
-userRouter.route("/check").get(UserAuth);
-userRouter.route("/logout").post(UserLogout);
